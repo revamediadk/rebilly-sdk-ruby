53c4df30924cf26ab22b04e61e6d7337a8a4e7b2
Tarek El-Sayyad <tarek.elsayyad@gmail.com>
2024-09-30 11:28:04 +0300

    allow custom fields to be sent

--- lib/rebilly_sdk/models/resource_custom_fields.rb
+++ lib/rebilly_sdk/models/resource_custom_fields.rb
@@ -12,107 +12,93 @@ Swagger Codegen version: 3.0.33-SNAPSHOT
 require 'date'
 
 module RebillySdk
-  # Custom Fields list as a map `{\"custom field name\": \"custom field value\", ...}`. The format must follow the saved format (see Custom Fields section for the formats). 
+  # Custom Fields list as a map `{"custom field name": "custom field value", ...}`. The format must follow the saved format (see Custom Fields section for the formats). 
   class ResourceCustomFields
     # Attribute mapping from ruby-style variable name to JSON key.
     def self.attribute_map
-      {
-      }
+      @attribute_map ||= {}
     end
 
     # Attribute type mapping.
     def self.openapi_types
-      {
-      }
+      @openapi_types ||= {}
     end
 
     # List of attributes with nullable: true
     def self.openapi_nullable
-      Set.new([
-      ])
+      @openapi_nullable ||= Set.new
     end
   
-    # Initializes the object
+    # Initializes the object with any fields
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
         fail ArgumentError, "The input argument (attributes) must be a hash in `RebillySdk::ResourceCustomFields` initialize method"
       end
 
-      # check to see if the attribute exists and convert string to symbol for hash key
-      attributes = attributes.each_with_object({}) { |(k, v), h|
-        if (!self.class.attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `RebillySdk::ResourceCustomFields`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
-        end
-        h[k.to_sym] = v
-      }
+      attributes.each do |key, value|
+        # Convert string keys to symbols and store attributes dynamically
+        self.class.attribute_map[key.to_sym] = key
+        self.class.openapi_types[key.to_sym] = value.class.to_s
+        instance_variable_set("@#{key}", value)
+      end
     end
 
-    # Show invalid properties with the reasons. Usually used together with valid?
-    # @return Array for valid properties with the reasons
+    # Dynamically define getter methods for each field
+    def method_missing(method, *args, &block)
+      attribute = method.to_s
+      if attribute.end_with?("=")
+        attribute_name = attribute.chomp("=").to_sym
+        value = args.first
+        self.class.attribute_map[attribute_name] = attribute_name
+        self.class.openapi_types[attribute_name] = value.class.to_s
+        instance_variable_set("@#{attribute_name}", value)
+      elsif self.class.attribute_map.key?(method)
+        instance_variable_get("@#{method}")
+      else
+        super
+      end
+    end
+
+    # Show invalid properties with the reasons.
     def list_invalid_properties
-      invalid_properties = Array.new
+      invalid_properties = []
       invalid_properties
     end
 
-    # Check to see if the all the properties in the model are valid
-    # @return true if the model is valid
+    # Check if all the properties in the model are valid.
     def valid?
       true
     end
 
     # Checks equality by comparing each attribute.
-    # @param [Object] Object to be compared
     def ==(o)
       return true if self.equal?(o)
-      self.class == o.class
-    end
-
-    # @see the `==` method
-    # @param [Object] Object to be compared
-    def eql?(o)
-      self == o
+      self.class == o.class && self.to_hash == o.to_hash
     end
 
     # Calculates hash code according to all attributes.
-    # @return [Integer] Hash code
     def hash
-      [].hash
+      self.to_hash.hash
     end
 
     # Builds the object from hash
-    # @param [Hash] attributes Model attributes in the form of hash
-    # @return [Object] Returns the model itself
     def self.build_from_hash(attributes)
       new.build_from_hash(attributes)
     end
 
     # Builds the object from hash
-    # @param [Hash] attributes Model attributes in the form of hash
-    # @return [Object] Returns the model itself
     def build_from_hash(attributes)
       return nil unless attributes.is_a?(Hash)
-      self.class.openapi_types.each_pair do |key, type|
-        if type =~ /\AArray<(.*)>/i
-          # check to ensure the input is an array given that the attribute
-          # is documented as an array but the input is not
-          if attributes[self.class.attribute_map[key]].is_a?(Array)
-            self.send("#{key}=", attributes[self.class.attribute_map[key]].map { |v| _deserialize($1, v) })
-          end
-        elsif !attributes[self.class.attribute_map[key]].nil?
-          self.send("#{key}=", _deserialize(type, attributes[self.class.attribute_map[key]]))
-        elsif attributes[self.class.attribute_map[key]].nil? && self.class.openapi_nullable.include?(key)
-          self.send("#{key}=", nil)
-        end
+      attributes.each do |key, value|
+        self.class.attribute_map[key.to_sym] = key
+        self.class.openapi_types[key.to_sym] = value.class.to_s
+        instance_variable_set("@#{key}", value)
       end
-
       self
     end
 
     # Deserializes the data based on type
-    # @param string type Data type
-    # @param string value Value to be deserialized
-    # @return [Object] Deserialized data
     def _deserialize(type, value)
       case type.to_sym
       when :DateTime
@@ -126,62 +112,47 @@ module RebillySdk
       when :Float
         value.to_f
       when :Boolean
-        if value.to_s =~ /\A(true|t|yes|y|1)\z/i
-          true
-        else
-          false
-        end
+        !!value
       when :Object
-        # generic object (usually a Hash), return directly
         value
-      when /\AArray<(?<inner_type>.+)>\z/
-        inner_type = Regexp.last_match[:inner_type]
+      when /\AArray<(.*)>\z/
+        inner_type = Regexp.last_match[1]
         value.map { |v| _deserialize(inner_type, v) }
-      when /\AHash<(?<k_type>.+?), (?<v_type>.+)>\z/
-        k_type = Regexp.last_match[:k_type]
-        v_type = Regexp.last_match[:v_type]
+      when /\AHash<(.+?), (.+)>\z/
+        k_type, v_type = Regexp.last_match[1], Regexp.last_match[2]
         {}.tap do |hash|
           value.each do |k, v|
             hash[_deserialize(k_type, k)] = _deserialize(v_type, v)
           end
         end
-      else # model
-        RebillySdk.const_get(type).build_from_hash(value)
+      else
+        value
       end
     end
 
-    # Returns the string representation of the object
-    # @return [String] String presentation of the object
+    # Converts the object to a string representation.
     def to_s
       to_hash.to_s
     end
 
-    # to_body is an alias to to_hash (backward compatibility)
-    # @return [Hash] Returns the object in the form of hash
+    # Alias to to_hash for backward compatibility.
     def to_body
       to_hash
     end
 
-    # Returns the object in the form of hash
-    # @return [Hash] Returns the object in the form of hash
+    # Converts the object to a hash.
     def to_hash
       hash = {}
-      self.class.attribute_map.each_pair do |attr, param|
-        value = self.send(attr)
-        if value.nil?
-          is_nullable = self.class.openapi_nullable.include?(attr)
-          next if !is_nullable || (is_nullable && !instance_variable_defined?(:"@#{attr}"))
-        end
+      self.class.attribute_map.each do |attr, param|
+        value = instance_variable_get("@#{attr}")
+        next if value.nil?
 
         hash[param] = _to_hash(value)
       end
       hash
     end
 
-    # Outputs non-array value in the form of hash
-    # For object, use to_hash. Otherwise, just return the value
-    # @param [Object] value Any valid value
-    # @return [Hash] Returns the value in the form of hash
+    # Outputs non-array value in the form of hash.
     def _to_hash(value)
       if value.is_a?(Array)
         value.compact.map { |v| _to_hash(v) }
@@ -189,10 +160,16 @@ module RebillySdk
         {}.tap do |hash|
           value.each { |k, v| hash[k] = _to_hash(v) }
         end
-      elsif value.respond_to? :to_hash
+      elsif value.respond_to?(:to_hash)
         value.to_hash
       else
         value
       end
-    end  end
+    end
+
+    # Supports dynamic getters and setters.
+    def respond_to_missing?(method, include_private = false)
+      self.class.attribute_map.key?(method) || super
+    end
+  end
 end
